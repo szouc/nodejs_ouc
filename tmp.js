4afb9636283d739e9b3b8acff774d3e3d04be82e
szouc <szouc@126.com>
2017-05-23 12:21:59 +0800

    Add the difference between Events and Promises

--- tmp.js
+++ tmp.js
@@ -1,24 +1,27 @@
-const Events = require('events');
+// Promises 和 事件发布/订阅的区别
 
-class Stream extends Events {
-  constructor(){
-    super(Events);
-  }
-  
-}
+const EventsEmitter = require('events');
 
-const emitter = new Stream();
+const emitter = new EventsEmitter();
 
-// emitter.on("done", (data) => {
-//   console.log(data + " done");
-// })
+const promise = new Promise(function(resolve, reject) {
+  console.log("promise");
+  resolve();
+});
+
+emitter.emit("done", "emit_ignore") // 触发事件，但是没有事件被订阅
 
 emitter.on("done", (data) => {
-  setImmediate(() => {
-    console.log(data + " done");
+  console.log(data + " done");
+  promise.then(function() {     // 进入微指令队列（mirco），延迟执行。
+    console.log("then in done");  
   });
 });
 
-emitter.emit("done", "emit")
+emitter.emit("done", "emit")    // 触发事件，事件被订阅，事件侦听器立即执行（同步）
 
 console.log("This is script.")
+
+promise.then(function() {       // 进入微指令队列（mirco），延迟执行。
+  console.log("then");
+});
